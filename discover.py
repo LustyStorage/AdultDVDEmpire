2b724e78b696d2128072d2f19811c852c6f2f650
LustyStorage <149494078+LustyStorage@users.noreply.github.com>
2024-06-18 14:48:27 +0600

    Update discover.py

--- discover.py
+++ discover.py
@@ -1,14 +1,16 @@
 import re
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
 import json
 import pymongo
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
-# Setup Selenium WebDriver
-service = Service(ChromeDriverManager().install())
-driver = webdriver.Chrome(service=service)
+options = Options()
+options.add_argument('--headless')
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-dev-shm-usage')
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
 # Base URL
 base_url = "https://www.adultdvdempire.com/all-dvds.html?unlimited=0&media=2&page={}"
