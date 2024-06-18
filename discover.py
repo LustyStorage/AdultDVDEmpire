551597abbbb37b20e00413effd9e3fab073faddf
LustyStorage <149494078+LustyStorage@users.noreply.github.com>
2024-06-18 14:54:17 +0600

    Update discover.py

--- discover.py
+++ discover.py
@@ -2,6 +2,7 @@ import re
 import json
 import pymongo
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
